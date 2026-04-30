a8d1042e111ff896dd1ca524e74eb1b66d15553e
JayJay-Ray <jayespartinez9@gmail.com>
2026-04-30 11:26:36 +0800

    Implement clearScreen method for terminal clearing
    Add clearScreen method to clear terminal output based on OS.

--- Main.java
+++ Main.java
@@ -182,7 +182,16 @@ public class Main {
   }
   //method for clearing the terminal once the user decided to make another cpu scheduling.
   public static void clearScreen() {
+    try {
+      String os = System.getProperty("os.name");
+      if(os.contains("Windows")) {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+      } else {
         System.out.print("\033[H\033[2J");
         System.out.flush();
+      }
+    }catch(Exception e) {
+      System.out.println("Error");
     }
+  }
 }
